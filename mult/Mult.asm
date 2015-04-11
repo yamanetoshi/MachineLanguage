62847cf34025fb98b7468c8f1cf9d628cd5dcc1f
YAMANE Toshiaki <yamanetoshi@gmail.com>
2015-04-11 15:07:36 +0900

    fix Mult.asm

--- mult/Mult.asm
+++ mult/Mult.asm
@@ -7,3 +7,25 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+    @i
+    M=0
+    @2
+    M=0
+(LOOP)
+    @i
+    D=M
+    @1
+    D=M-D
+    @END
+    D;JLE
+    @0
+    D=M
+    @2
+    M=D+M
+    @i
+    M=M+1
+    @LOOP
+    0;JMP
+(END)
+    @END
+    0;JMP
